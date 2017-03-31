e5174a638b11198dd60cd7460d6813d8c8275bac
Simas <ab.simas@gmail.com>
2017-03-31 22:23:53 +0300

    Handle transaction amount and account balance modifications.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ApplicationRecord
-  has_many :sent, class_name: "Transaction", foreign_key: "sender_id"
-  has_many :received, class_name: "Transaction", foreign_key: "recipient_id"
+  has_many :sent, class_name: "Transaction", foreign_key: "sender_id", dependent: :destroy
+  has_many :received, class_name: "Transaction", foreign_key: "recipient_id", dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,10 +7,35 @@ class Transaction < ApplicationRecord
   validates :amount, presence: true
 
   validate :sender_and_recipient_differ
+  validate :sufficient_sender_balance
 
-  def sender_and_recipient_differ
-    if sender == recipient
-      errors.add(:sender, 'cannot create a transaction to self')
+  before_save :modify_balances
+  before_destroy :reset_balances
+
+  private
+    def sender_and_recipient_differ
+      if sender == recipient
+        errors.add(:sender, 'cannot create a transaction to self')
+      end
     end
-  end
+
+    def sufficient_sender_balance
+      if sender.balance < amount
+        errors.add(:sender, 'balance is lower than the requested transaction amount')
+      end
+    end
+
+    def modify_balances
+      sender.balance -= amount
+      sender.save!
+      recipient.balance += amount
+      recipient.save!
+    end
+
+    def reset_balances
+      sender.balance += amount
+      sender.save!
+      recipient.balance -= amount
+      recipient.save!
+    end
 end
