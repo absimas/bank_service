9e31a18462b80f22d7091bcfcdf2f2512e54643e
Simas <ab.simas@gmail.com>
2017-03-31 11:00:43 +0300

    Validate sender and recipient so they're not equal.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,16 @@
 class Transaction < ApplicationRecord
-  belongs_to :sender, class_name: "Account"
-  belongs_to :recipient, class_name: "Account"
+  belongs_to :sender, class_name: 'Account'
+  belongs_to :recipient, class_name: 'Account'
 
   validates :sender, presence: true
   validates :recipient, presence: true
   validates :amount, presence: true
+
+  validate :sender_and_recipient_differ
+
+  def sender_and_recipient_differ
+    if sender == recipient
+      errors.add(:sender, 'cannot create a transaction to self')
+    end
+  end
 end
