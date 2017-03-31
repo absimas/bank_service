a0f618a25fc72a1c460aea28a1beb4ff3ccce37c
Simas <ab.simas@gmail.com>
2017-03-31 11:01:30 +0300

    Redirect quietly (without changing the browser's URL)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :accounts
   get '/transactions/sender/:id', to: 'transactions#sender'
   get '/transactions/recipient/:id', to: 'transactions#recipient'
-  get '/accounts/:id/sent', to: redirect('/transactions/sender/%{id}')
-  get '/accounts/:id/received', to: redirect('/transactions/recipient/%{id}')
+  get '/accounts/:id/sent', to: 'transactions#sender'
+  get '/accounts/:id/received', to: 'transactions#recipient'
 
 end
